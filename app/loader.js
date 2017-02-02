fc7ae061e25dfb072b335a2669806e2b2fcf686f
ihanchuk <mfc2005@ukr.net>
2017-02-02 23:16:03 +0200

    first test

--- app/loader.js
+++ app/loader.js
@@ -1,5 +1,12 @@
 var angular = require("angular");
 var app=angular.module("app",[]);
+app.controller('PhoneListCtrl', ['$scope', function($scope){
+    $scope.phones = [
+        {name: "Nexus S", snippet: "Fast..."},
+        {name: "Motorola XOOM...", snippet: "The Next...."},
+        {name: "MOTOROLA XOOM...", snippet: "The Next, Next..."}
+    ];
+}]);
 
 module.exports = function(){
     return app;
