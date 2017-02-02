40fc78331a98a8bec07269815c35026536f6785f
ihanchuk <mfc2005@ukr.net>
2017-02-02 21:23:24 +0200

    merge conflict

--- karma.conf.js
+++ karma.conf.js
@@ -31,15 +31,12 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     // preprocessors: processors,
-<<<<<<< HEAD
 
     preprocessors: {
       // add webpack as preprocessor
       './app/test/*': ['webpack'],
       
     },
-=======
->>>>>>> 3138adb41c8fb965b709e01afd04b679a4aeaeda
 
     // list of files to exclude
     exclude: [
